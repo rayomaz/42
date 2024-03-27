ba96d719e69d1dabe5b11f9821ff932c19649985
Rayan Mazouz <rayan.mazouz@colorado.edu>
2024-03-27 14:15:01 -0700

    Setting up call to 42 from matlab

---
+++ main.m
@@ -0,0 +1,10 @@
+%% Calling 42 C code through Matlab interface
+% Rayan Mazouz, Marco Quadrelli
+% Verification: Orbits and Attitude
+
+clc; clear; close;
+
+% Call 42
+output = system('./42 InOut');
+
+
