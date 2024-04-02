357b65bbc773884a1d1eeda4a123aba43693f11f
Rayan Mazouz <rayan.mazouz@colorado.edu>
2024-04-02 09:48:08 -0700

    Plot some data from .42 objects

--- main.m
+++ main.m
@@ -2,26 +2,53 @@
 % Rayan Mazouz, Marco Quadrelli
 % Verification: Orbits and Attitude
 
-clc; clear; close;
+clc; clear; close all;
 
 % Call 42
-output = system('./42 InOut');
+output = system('./42 Custom');
 
+% Load data
+load ./Custom/time.42
+load ./Custom/PosN.42
+load ./Custom/VelN.42
+load ./Custom/qbn.42
+load ./Custom/wbn.42
 
-% Save
-% for Irun=1:Nrun,
-% % Compute initial attitude
-% CRN = TRIAD(tvn(Irun,:),svn,[0 0 1],[1 0 0]);
-% qrn = C2Q(CRN);
-% % Write target to file
-% Outdata = [TrgRA(Irun) TrgDec(Irun)];
-% save -ascii ./MOMBIAS/TargetRaDec.inp Outdata
-% % Write initial attitude to file
-% line = sprintf('%f %f %f %f ! Quaternion\n', qrn(1),qrn(2),qrn(3),qrn(4));
-% OverwriteLineInFile('./MOMBIAS/GLAST.inp',21,line);
-% % Run 42 for three days.
-% system('./42 MOMBIAS');
-% % Record pointing histogram.
-% load ./MOMBIAS/AngleToGo.42
-% [HistCount(Irun,:),HistAng(Irun,:)] = hist(AngleToGo,20);
-% end
+% Plot eci motion
+figure
+grid on
+hold on
+plot(time, PosN, 'LineWidth', 2)
+xlabel("Time (s)", 'Interpreter','latex', "FontSize", 25)
+ylabel("ECI Position [m]", 'Interpreter','latex', "FontSize", 25)
+legend("X", "Y", "Z", "Location", "best")
+
+% Plot eci velocity
+figure
+grid on
+hold on
+plot(time, VelN, 'LineWidth', 2)
+xlabel("Time (s)", 'Interpreter','latex', "FontSize", 25)
+ylabel("ECI Velocity [m/s]", 'Interpreter','latex', "FontSize", 25)
+legend("X", "Y", "Z", "Location", "best")
+
+% Plot quaternions
+figure
+grid on
+hold on
+plot(time, qbn, 'LineWidth', 2)
+xlabel("Time (s)", 'Interpreter','latex', "FontSize", 25)
+ylabel("Quaternion", 'Interpreter','latex', "FontSize", 25)
+legend("q_1", "q_2", "q_3", "q_4", "Location", "best")
+
+% Plot Euler angles
+plotEulerAngles(time, qbn)
+
+% Plot body rate
+% figure
+% grid on
+% hold on
+% plot(time, wbn, 'LineWidth', 2)
+% xlabel("Time (s)", 'Interpreter','latex', "FontSize", 25)
+% ylabel("Angular Velocity [m/s]", 'Interpreter','latex', "FontSize", 25)
+% legend("X", "Y", "Z", "Location", "best")
